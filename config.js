badba887f3fe78011e4988f35379fe110d470439
7Honor <writer.bars@gmail.com>
2026-05-28 16:24:29 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.RUNPILOT_CONFIG = {
+  SUPABASE_URL: "",
+  SUPABASE_PUBLISHABLE_KEY: "",
+  TABLE: "marathon_data"
+};
---
+++ sw.js
@@ -0,0 +1,1 @@
+// intentionally empty in v15 to avoid stale cache during development
